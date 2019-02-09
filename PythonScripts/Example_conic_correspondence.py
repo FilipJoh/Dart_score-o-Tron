13c25b1ff6e3bc1701e088a2ddc45ed7cf45cb21
Alexander Olsson <alexander.olsson@neodev.se>
2019-02-10 00:20:52 +0100

    planar homographies of conics (probably) functional

--- PythonScripts/Example_conic_correspondence.py
+++ PythonScripts/Example_conic_correspondence.py
@@ -5,7 +5,7 @@ import numpy as np
 import cv2
 
 class Conic(object):
-    def __init__(self, C, Z=0):
+    def __init__(self, C, Z):
         # Ax**2 + Bxy + Cy**2 + Dx + Ey + F = 0.
         self.C = C
         self.Z = Z
@@ -14,32 +14,43 @@ class Conic(object):
         # Given: p * M * p.T = 0
         pass
     
-    def pointwise_transform(self, H):
-        new_C = np.inv(H).T.dot(self.C).dot(np.inv(H))
+    def planar_transform(self, R1, t1, R2, t2):
+        n = np.expand_dims(np.asarray(((0),
+                                       (0),
+                                       (1))), axis=1)
+        
+        H_12 = R1.dot(np.eye(3) - ((t2  - t1).dot(n.T))/self.Z).dot(R2.T)
+        new_C = np.linalg.inv(H_12).T.dot(self.C).dot(np.linalg.inv(H_12))
         
         discriminant = 2*new_C[0, 1]**2 - 4*new_C[0, 0]*new_C[1, 1]
-        if np.det(new_C) == 0:
+        if np.linalg.det(new_C) == 0:
             ## Degenerate
             new_conic = None
         elif discriminant < 0:
             # Ellipse
-            new_conic = Ellipse(0,0,0,0,0)
+            new_conic = Ellipse()
+            new_conic.C = new_C
+            new_conic.Z = self.Z
+            new_conic.implicit_coefficients = {'A': new_C[0,0],
+                                             'B': new_C[0,1]*2,
+                                             'C': new_C[1,1],
+                                             'D': new_C[0,2]*2,
+                                             'E': new_C[1,2]*2,
+                                             'F': new_C[2,2]}
+            new_conic.set_parametrization()
+    
         elif discriminant == 0:
             # Parabola
             new_conic = None
         elif discriminant > 0:
             # Hyperbola
             new_conic = None
-        
-        new_conic.C = new_C
-        new_conic.Z = self.Z
-        new_conic.__set_parametrization()
         return new_conic
     
-    def __get_parametrization():
+    def __get_parametrization(self):
         pass
     
-    def __set_parametrization():
+    def set_parametrization(self):
         pass
     
     def plot(self):
@@ -64,28 +75,34 @@ class Conic(object):
         
 
 class Ellipse(Conic):
-    def __init__(self, C_x, C_y, R_x, R_y, theta, Z=0):
+    def __init__(self, C_x=0, C_y=0, R_x=1, R_y=1, theta=0, Z=1):
         self.C_x = C_x
         self.C_y = C_y
         self.R_x = R_x
         self.R_y = R_y
         self.theta = theta
         
-        self.__get_parametrization()
+        self.get_parametrization()
         matrix_representation = np.asarray(((self.implicit_coefficients['A'], self.implicit_coefficients['B']/2, self.implicit_coefficients['D']/2),
                                              (self.implicit_coefficients['B']/2, self.implicit_coefficients['C'], self.implicit_coefficients['E']/2),
                                              (self.implicit_coefficients['D']/2, self.implicit_coefficients['E']/2, self.implicit_coefficients['F'])))
         
         super().__init__(matrix_representation, Z)
         
-    def __get_parametrization(self):
+    def get_parametrization(self):
         A = (self.R_y**2) * (np.cos(self.theta)**2) + (self.R_x**2) * (np.sin(self.theta))**2
         B = 2 * np.sin(self.theta) * np.cos(self.theta) * (self.R_y**2 - self.R_x**2)
         C = (self.R_y**2) * (np.sin(self.theta))**2 + (self.R_x**2) * (np.cos(self.theta))**2
         D = -(2*A*self.C_x + B*self.C_y)
         E = -(2*C*self.C_y + B*self.C_x)
         F = A*self.C_x**2 + B*self.C_x*self.C_y + C*self.C_y**2 - self.R_x**2*self.R_y**2
-        self.implicit_coefficients =  {'A': A, 'B': B, 'C': C, 'D': D, 'E': E, 'F': F}
+        
+        self.implicit_coefficients =  {'A': A,
+                                       'B': B,
+                                       'C': C,
+                                       'D': D,
+                                       'E': E,
+                                       'F': F}
     
     def set_parametrization(self):
         eq_2by2 = np.asarray(((self.implicit_coefficients['A'], self.implicit_coefficients['B']/2),
@@ -102,9 +119,15 @@ class Ellipse(Conic):
             self.theta +=  2*np.pi
         pass
     
-    def plot(self):
+    def plot(self, title):
         p = self.sample(1000)
-        plt.scatter(p[0,:], p[1,:], label='parametrical')  
+        plt.figure()
+        plt.scatter(p[0,:], p[1,:], label='parametrical')
+        plt.axhline(0, color='black')
+        plt.axvline(0, color='black')
+        plt.axis([-10, 10, -10, 10])
+        plt.grid('on')
+        plt.title(title)
 
     def sample(self, npoints=10):
         # Generate uniformely distributed angles
@@ -128,32 +151,42 @@ class Ellipse(Conic):
             points[1, i] = y_translated
         
         return points
-    
-    def prent(self):
-        print('C_x: {}'.format(self.C_x))
-        print('C_y: {}'.format(self.C_y))
-        print('R_x: {}'.format(self.R_x))
-        print('R_y: {}'.format(self.R_y))
-        print('Theta: {}'.format(self.theta))
 
 
-e = Ellipse(7, 1, 5, 2, 0.2*np.pi)
-print('before')
-e.prent()
-plt.figure()
-e.plot()
-plt.title('before parameter extraction')
-print('after')
-e.set_parametrization()
-e.prent()
-plt.figure()
-e.plot()
-plt.title('after parameter extraction')
+e_1 = Ellipse(0, 5, 2, 2, 0.0*np.pi)
+e_1.plot('Before homography')
         
-#P1 = np.asarray(((1, 0, 0, 0), (0, 1, 0, 0), (0, 0, 1, 0)))
-#
-#K1 = []
-#R1 = []
-#t1 = []
-#
-#P2 = []
+
+R1 = np.asarray(((1, 0, 0),
+                 (0, 1, 0),
+                 (0, 0, 1)))
+
+t1 = np.expand_dims(np.asarray(((0),
+                                (0),
+                                (0))), axis=1)
+
+##No rotation
+#R2 = np.asarray(((1, 0, 0),
+#                 (0, 1, 0),
+#                 (0, 0, 1)))
+
+#Rotation about z-axis
+theta_rot = np.pi/2
+R2 = np.asarray(((np.cos(theta_rot), -np.sin(theta_rot), 0),
+                 (np.sin(theta_rot), np.cos(theta_rot), 0),
+                 (0, 0, 1)))
+
+##Rotation about y-axis
+#phi_rot = np.pi/16
+#R2 = np.asarray(((np.cos(phi_rot), 0, np.sin(phi_rot)),
+#                 (0, 1, 0),
+#                 (-np.sin(phi_rot), 0, np.cos(phi_rot))))
+
+t2 = np.expand_dims(np.asarray(((0),
+                                (0),
+                                (0))), axis=1)
+
+
+e_2 = e_1.planar_transform(R1, t1, R2, t2)
+e_2.plot('After homography')
+
